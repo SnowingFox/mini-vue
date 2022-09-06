046c5ccac537af5d0997f892e58a820e46e7b50e
snowingfox <1503401882@qq.com>
2022-09-06 12:53:34 +0800

    chore: init runtime-core

--- alias.ts
+++ alias.ts
@@ -5,4 +5,5 @@ const r = (p: string) => resolve(__dirname, p)
 export const alias: Record<string, string> = {
   '@mini-vue/shared': r('./packages/shared/src/'),
   '@mini-vue/reactivity': r('./packages/reactivity/src/index.ts'),
+  '@mini-vue/runtime-core': r('./packages/runtime-core/src/'),
 }
