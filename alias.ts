7e4a21ea243442e1df0db5a160773530af3fa20d
snowingfox <1503401882@qq.com>
2022-10-08 20:27:32 +0800

    feat: init playground

--- alias.ts
+++ alias.ts
@@ -6,4 +6,5 @@ export const alias: Record<string, string> = {
   '@mini-vue/shared': r('./packages/shared/src/'),
   '@mini-vue/reactivity': r('./packages/reactivity/src/index.ts'),
   '@mini-vue/runtime-core': r('./packages/runtime-core/src/'),
+  '@mini-vue/playground': r('./packages/playground/src/'),
 }
